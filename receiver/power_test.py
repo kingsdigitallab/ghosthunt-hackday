6de4e5e3ec7ee8fbbd027fa8a3c0e803de33d6e0
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-02-06 16:24:06 +0000

    added unicorn display test

--- receiver/power_test.py
+++ receiver/power_test.py
@@ -1,36 +1,14 @@
-from rtlsdr import *
-from pylab import *
-import time
-import math
 import unicornhat as unicorn
 import os
 import datetime
 
-# Radio test script for Ghost Hunt Hackday
-# Written by Brian Maher 31/1/2018
-# Modified by Elliott Hall
+#unicorn hat light test
 
 # Init Unicorn
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
 unicorn.brightness(0.2)
 width,height=unicorn.get_shape()
-
-sdr = RtlSdr()
-sdr.sample_rate = 3.2e6
-sdr.center_freq = 885e5
-sdr.gain = 20
-
-if not os.path.exists('power.log'):
-    with open('power.log', 'wt') as f:
-        f.write("Power Script Log\n\n")
-
-def _get_pixel_count(db):
-    # crude
-    count = 64
-    db = int(db)
-    count = count + db
-    return count
 
 def _display_unicorn(db):
     # turn db into % and display in unicorn
@@ -55,15 +33,7 @@ def _display_unicorn(db):
                 unicorn.set_pixel(x,y,0,0,0)
             cur_pixel = cur_pixel + 1
 
-
-
-
 while True:
-    unicorn.clear()
-    samples = sdr.read_samples(500e3)
-    db = 10*log10(var(samples))
-    print 'relative power: %0.1f dB' % (10*log10(var(samples)))
-    with open('power.log', 'ab') as f:
-        f.write("[{}] {} dB\n".format(datetime.datetime.now(),  (10*log10(var(samples)))))
+    db = -23
     _display_unicorn(db)
     unicorn.show()
